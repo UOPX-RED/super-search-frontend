bd1cab9bbaa70d0bb08d55258979fee6e84ecd54
Hector Silva-Robles <hector.silva-robles@skillmore.com>
2025-03-11 12:08:42 -0700

    update proxy

--- SUPER-SEARCH-FRONTEND/vite.config.ts
+++ SUPER-SEARCH-FRONTEND/vite.config.ts
@@ -14,10 +14,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: process.env.VITE_BACKEND_URL || "http://localhost:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
-    }
-  }
+    },
+  },
 });
